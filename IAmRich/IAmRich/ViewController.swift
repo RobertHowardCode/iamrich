0ed541cdd1a076b1d1a4198715ebf12c86e60e15
Robert Howard <robertbhoward.oa@gmail.com>
2019-01-23 17:31:02 -0800

    Basic 1 screen application complete in storyboard, iOS Interface Builder. Image Assets in, images and app icons, as well as side loaded onto phone.

---
+++ IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Work on 1/23/19.
+//  Copyright © 2019 robertbhoward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
